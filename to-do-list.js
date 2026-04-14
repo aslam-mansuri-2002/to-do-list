c2150f21dbc06ac7734ccc828ffe1a33e33c48d3
aslam-mansuri-2002 <dholkadholka56@gmail.com>
2026-04-14 11:08:40 +0530

    added local storage functionality

--- to-do-list.js
+++ to-do-list.js
@@ -1,72 +1,69 @@
 // to do list
+let my_input = document.querySelector(".my-input");
+let my_btn = document.querySelector(".my-btn");
+let abc = document.querySelector(".abc");
 
-// let div2 = document.getElementsByClassName("my-div2")[0];
-let my_input = document.getElementsByClassName("my-input")[0];
-let my_btn = document.getElementsByClassName("my-btn")[0];
-let div = document.getElementsByClassName("my-div")[0]
-let abc = document.getElementsByClassName("abc")[0];
+// 1. LocalStorage se tasks load karein ya khali array lein
+let tasks = JSON.parse(localStorage.getItem("myTasks")) || [];
 
-my_btn.addEventListener("click",function(){
-    let div2 = document.createElement("div")
-    let h4 = document.createElement("h4");
-    let del_btn = document.createElement("button");
-    h4.innerText=my_input.value;
-    del_btn.innerText ="delete";
+// Page load hone par purane tasks dikhayein
+renderTasks();
 
-    // style
-    h4.style.display="inline-block";
-    h4.style.margin="0px";
-    div2.style.display="flex";
-    div2.style.justifyContent ="space-between"
-    div2.style.margin="10px 20px 10px 20px";
+function renderTasks() {
+    // Pehle se maujood task-items ko remove karein (input field wale div ko chod kar)
+    let existingTasks = document.querySelectorAll(".task-item");
+    existingTasks.forEach(item => item.remove());
 
+    tasks.forEach((task, index) => {
+        let div2 = document.createElement("div");
+        div2.classList.add("task-item"); // Class for identification
+        
+        let h4 = document.createElement("h4");
+        let del_btn = document.createElement("button");
+        
+        h4.innerText = task;
+        del_btn.innerText = "delete";
 
-    del_btn.addEventListener("click",function(){
-        div2.remove();
-    })
-    if(my_input.value===""){
-        alert("please Enter value");
-    }
-    else{
+        // basic styles
+        h4.style.display = "inline-block";
+        h4.style.margin = "0px";
+        div2.style.display = "flex";
+        div2.style.justifyContent = "space-between";
+        div2.style.margin = "10px 20px 10px 20px";
+
+        // Delete function
+        del_btn.addEventListener("click", function () {
+            tasks.splice(index, 1); // Array se delete karein
+            saveAndRender();        // Save aur refresh karein
+        });
 
         div2.appendChild(h4);
         div2.appendChild(del_btn);
         abc.appendChild(div2);
-        my_input.value = "";
-    }    
-})
+    });
+}
 
-// enter dabane se..
+function saveAndRender() {
+    localStorage.setItem("myTasks", JSON.stringify(tasks));
+    renderTasks();
+}
 
-my_input.addEventListener("keydown", function (a, b) {
-    if (a.key === "Enter") {
-        let div2 = document.createElement("div")
-        let h4 = document.createElement("h4");
-        let del_btn = document.createElement("button");
-        h4.innerText = my_input.value;
-        del_btn.innerText = "delete";
-
-        // style
-        h4.style.display = "inline-block";
-        h4.style.margin = "0px";
-        div2.style.display = "flex";
-        div2.style.justifyContent = "space-between"
-        div2.style.margin = "10px 20px 10px 20px";
-
-
-        del_btn.addEventListener("click", function () {
-            h4.remove();
-            del_btn.remove();
-        })
-        if (my_input.value === "") {
-            alert("please Enter value");
-        }
-        else {
-
-            div2.appendChild(h4);
-            div2.appendChild(del_btn);
-            abc.appendChild(div2);
-            my_input.value = "";
-        }
+function addTask() {
+    if (my_input.value.trim() === "") {
+        alert("Please Enter value");
+        return;
     }
-})
+    tasks.push(my_input.value); // Array mein add karein
+    my_input.value = "";
+    saveAndRender();            // Save aur refresh karein
+}
+
+// Click par add
+my_btn.addEventListener("click", addTask);
+
+// Enter dabane par add
+my_input.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        addTask();
+    }
+});
